068e6e0677fd8f3f28b05a75b2c419065d13c1ba
kon <schubert.konstantin@gmail.com>
2015-01-08 03:16:53 +0100

    started work on user interface

---
+++ ui.py
@@ -0,0 +1,35 @@
+import kivy
+import subprocess
+kivy.require("1.8.0")
+from kivy.app import App
+from kivy.uix.button import Button
+from kivy.uix.label import Label
+from kivy.uix.gridlayout import GridLayout
+
+
+class SortPictures(App):
+
+
+    def sortNow(self,instance):
+        print "test"
+
+    def build(self):
+        layout = GridLayout(cols=2, rows=3)
+        button = Button(text='Sort Now')
+        button.bind(on_press=self.sortNow)
+        label1 = Label(text='placeholder1')
+        label2 = Label(text='placeholder2')
+        label3 = Label(text='placeholder3')
+        label4 = Label(text='placeholder4')
+        label5 = Label(text='placeholder5')
+        layout.add_widget(label1)
+        layout.add_widget(label2)
+        layout.add_widget(label3)
+        layout.add_widget(button)
+        layout.add_widget(label4)
+        layout.add_widget(label5)
+        return layout
+
+
+if __name__ == '__main__':
+    SortPictures().run()
