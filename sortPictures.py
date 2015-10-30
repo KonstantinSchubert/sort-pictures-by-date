08289d1ceb16558e3fcb4985013f3a81fda53127
kon <schubert.konstantin@gmail.com>
2015-10-30 21:00:14 +0100

    made heuristic more powerful, hopefully not TOO powerful...

--- sortPictures.py
+++ sortPictures.py
@@ -1,36 +1,38 @@
 #!/usr/bin/python
 
-from datetime import datetime 
+from datetime import datetime
 from PIL import Image
 import glob
 import os
 
 
 def getDate(url):
-	try: 
-		img = Image.open(url)		
+	try:
+		img = Image.open(url)
 		exif = img._getexif()
 		if 36867 in exif:
+			print "fount1"
 			return  datetime.strptime(exif[36867],'%Y:%m:%d  %H:%M:%S').date()
 		if 34853 in exif:
-			# looking for "date modified"	
+			print ' looking for "date modified" '
 			return  datetime.strptime(str(exif[34853][29]),'%Y:%m:%d').date()
-	except Exception as e:
-		print "Problem with exif data:"+str(e)
-		print "Now using file creation time"
-		return datetime.fromtimestamp(os.stat(url).st_mtime)
+	except Exception:
+		pass
+	print "Problem with exif data"
+	print "Now using file creation time"
+	return datetime.fromtimestamp(os.stat(url).st_mtime)
 
-		
+
 
 
 basedir = "../Bilder/"
 os.chdir(".")
 
-for ending in ["jpeg","JPG","avi","AVI","mp4","jpg"]:
+for ending in ["jpeg","JPG","avi","AVI","mp4","jpg", "JPG", "MOV", "PNG"]:
 	for fileName in glob.glob("*."+ending):
 		print "sorting "+ fileName
 		try:
-		    date = getDate(fileName)	    
+		    date = getDate(fileName)
 		    targetdir = str(basedir+date.strftime("%Y/%m %B")+"/")
 		    if not os.path.isdir(targetdir):
 		    	os.makedirs(targetdir)
